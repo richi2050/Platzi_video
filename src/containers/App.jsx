f4985e5143d67955597578765a9163e5361dc0fc
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2019-11-21 15:46:52 -0600

    se agrego el componente search y estilos al mismo

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Header from '../components/Header';
-import '../assets/styles/App.scss';
+import Header from "../components/Header";
+import Search from "../components/Search";
+import "../assets/styles/App.scss";
 
 const App = () => (
-	<div className="App">
-	<Header />
-
-	</div>
+  <div className="App">
+    <Header />
+    <Search />
+  </div>
 );
 
 export default App;
