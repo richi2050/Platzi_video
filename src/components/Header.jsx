819102da1f9f02f839aaf141bcd8d41ec8d84644
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2019-11-21 15:22:32 -0600

    se incluye lo que es el header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render(){
+		return (
+		<header className="header">
+			<img className="header__img" src="../assets/logo-platzi-video-BW2.png" alt="Platzi Video" />
+			<div className="header__menu">
+			<div className="header__menu--profile">
+				<img src="../assets/user-icon.png" alt="" />
+				<p>Perfil</p>
+			</div>
+			<ul>
+				<li><a href="/">Cuenta</a></li>
+				<li><a href="/">Cerrar Sesión</a></li>
+			</ul>
+			</div>
+		</header>
+		);
+	}
+
+}
+export default Header;
