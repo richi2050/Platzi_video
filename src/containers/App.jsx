d591d62a29abe4f0e1416889760e71967aa08c2f
richi2050 <ricardo.lugo.recillas@gmail.com>
2019-11-21 21:42:32 -0600

    Se genero el componente de categories, carousel, carouselItem

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouiselItem from "../components/CarouselItem";
+
 import "../assets/styles/App.scss";
 
 const App = () => (
   <div className="App">
     <Header />
     <Search />
+    <Categories>
+      <Carousel>
+        <CarouiselItem />
+        <CarouiselItem />
+        <CarouiselItem />
+        <CarouiselItem />
+        <CarouiselItem />
+        <CarouiselItem />
+      </Carousel>
+    </Categories>
   </div>
 );
 
