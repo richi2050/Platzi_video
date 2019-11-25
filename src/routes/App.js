6b9387076e75347762ff3926ba0fc28b1ea4f626
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2019-11-25 17:26:31 -0600

    se agfrego el notFound, y React.Fragment

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from '../containers/Home';
-import Login from '../containers/Login';
-import Register from '../containers/Register';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "../containers/Home";
+import Login from "../containers/Login";
+import Register from "../containers/Register";
+import NotFound from "../containers/NotFound";
 
 const App = () => (
-	<BrowserRouter>
-		<Switch>
-			<Route exact path='/' component ={Home} />
-			<Route exact path='/login' component ={Login} />
-			<Route exact path='/register' component ={Register} />
-		</Switch>
-	</BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route component={NotFound} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
+  </BrowserRouter>
 );
 export default App;
