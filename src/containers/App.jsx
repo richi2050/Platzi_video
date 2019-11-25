e49559ae089ff5228ecaf359d49bb81a0db15016
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2019-11-25 11:41:10 -0600

    se agrego consumir informacion de la fakeApi

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
@@ -7,33 +7,40 @@ import CarouiselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
 import "../assets/styles/App.scss";
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Search />
-    <Categories title="Titulo 1">
-      <Carousel>
-        <CarouiselItem />
-        <CarouiselItem />
-        <CarouiselItem />
-        <CarouiselItem />
-        <CarouiselItem />
-      </Carousel>
-    </Categories>
+const App = () => {
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: []});
 
-	<Categories title="Titulo 2">
-      <Carousel>
-        <CarouiselItem />
-        <CarouiselItem />
-        <CarouiselItem />
-        <CarouiselItem />
-        <CarouiselItem />
-		<CarouiselItem />
-      </Carousel>
-    </Categories>
+  useEffect(() => {
+    fetch("http://localhost:3000/initalState")
+      .then(response => response.json())
+      .then(data => setVideos(data));
+  }, []);
+ console.log(videos);
+  return (
+    <div className="App">
+      <Header />
+      <Search />
 
-	<Footer />
-  </div>
-);
+	{videos.mylist.length > 0 &&
+		<Categories title="Titulo 1">
+        <Carousel>
+          <CarouiselItem />
+        </Carousel>
+      </Categories>
+	}
+      
+
+      <Categories title="tendencias">
+        <Carousel>
+		{videos.trends.map( item => 
+			<CarouiselItem key={item.id} {...item}/>
+		)}
+        </Carousel>
+      </Categories>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
