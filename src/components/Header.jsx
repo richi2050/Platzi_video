b657e67b90b1c41dae2a512e05977d59c3ca876c
richi2050 <ricardo.lugo.recillas@gmail.com>
2019-11-25 21:20:50 -0600

    se agregaron los manejos de enlaces

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 import '../assets/styles/components/Header.scss';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
-	render(){
+	render() {
 		return (
-		<header className="header">
-			<img className="header__img" src={logo} alt="Platzi Video" />
-			<div className="header__menu">
-			<div className="header__menu--profile">
-				<img src={userIcon} alt="" />
-				<p>Perfil</p>
-			</div>
-			<ul>
-				<li><a href="/">Cuenta</a></li>
-				<li><a href="/">Cerrar Sesión</a></li>
-			</ul>
-			</div>
-		</header>
+			<header className="header">
+				<Link to='/'>
+					<img className="header__img" src={logo} alt="Platzi Video" />
+				</Link>
+				<div className="header__menu">
+					<div className="header__menu--profile">
+						<img src={userIcon} alt="" />
+						<p>Perfil</p>
+					</div>
+					<ul>
+						<li><a href="/">Cuenta</a></li>
+						<li><Link to="/login">Iniciar Sesión</Link></li>
+					</ul>
+				</div>
+			</header>
 		);
 	}
 
