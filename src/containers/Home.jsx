453f48db414f488c904e99a2f90f0b6124dc2533
richi2050 <ricardo.lugo.recillas@gmail.com>
2019-11-28 23:28:38 -0600

    Creando el Store de ReduxCreando el Store de Redux

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,47 +1,53 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouiselItem from '../components/CarouselItem';
 import '../assets/styles/App.scss';
-import useInitialState from '../hooks/useInitialState';
+/*import useInitialState from '../hooks/useInitialState';*/
 
-const API = 'http://localhost:3000/initalState';
 
-const Home = () => {
-  const initialState = useInitialState(API);
+/*const API = 'http://localhost:3000/initalState';*/
+
+const Home = ({ mylist, trends, originals}) => {
+  /*const initialState = useInitialState(API);*/
   return (
     <>
       <Search />
-      {initialState.mylist.length > 0 &&
+      {mylist.length > 0 &&
         <Categories title='Titulo 1'>
           <Carousel>
             <CarouiselItem />
           </Carousel>
         </Categories>
       }
-
       <Categories title='tendencias'>
         <Carousel>
-          {initialState.trends.map(item =>
+          {trends.map(item =>
             <CarouiselItem key={item.id} {...item} />
           )}
         </Carousel>
       </Categories>
-
-
-
       <Categories title='Originales '>
         <Carousel>
-          {initialState.originals.map(item =>
+          {originals.map(item =>
             <CarouiselItem key={item.id} {...item} />
           )}
         </Carousel>
       </Categories>
-
-
     </>
   );
 };
 
-export default Home;
+const mapStateToProps = state =>{
+  return {
+    mylist: state.myList,
+    trends: state.trends,
+    originals: state.originals 
+  }
+}
+
+
+//export default Home; /* forma de exportar un copmponente normal */
+export default connect(mapStateToProps, null)(Home); /* esta es la forma de exportatr un componente  ya conectado */
